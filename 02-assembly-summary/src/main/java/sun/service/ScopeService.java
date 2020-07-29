d1f06c9316fc3b3a5deca1848ea530af3cf34b08
sunchuizhe <2605587563@qq.com>
2020-07-29 18:24:42 +0800

    maven相关学习

---
+++ 02-assembly-summary/src/main/java/sun/service/ScopeService.java
@@ -0,0 +1,16 @@
+package sun.service;
+
+//import org.junit.Test;
+
+public class ScopeService {
+
+//    public static void main(String[] args) {
+//        System.out.println("???");
+//    }
+
+//    @Test
+//    public void f() {
+//        System.out.println("如果不加上，没啥毛病的呀");
+//    }
+
+}
---
+++ 02-assembly-summary/src/test/java/sun/service/ScopeServiceTest.java
@@ -0,0 +1,11 @@
+package sun.service;
+
+import org.junit.Test;
+
+public class ScopeServiceTest {
+
+    @Test
+    public void f() {
+        System.out.println(",,,");
+    }
+}
