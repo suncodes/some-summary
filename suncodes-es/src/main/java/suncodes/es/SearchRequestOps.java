4de8edb7d21fae7776fbdb81f8adb528b9f86800
sunchuizhe <2605587563@qq.com>
2023-05-06 17:39:01 +0800

    es - 查询操作

---
+++ suncodes-es/src/main/java/suncodes/es/SearchRequestOps.java
@@ -0,0 +1,50 @@
+package suncodes.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+import java.io.IOException;
+
+public class SearchRequestOps {
+    public static void main(String[] args) throws IOException {
+        searchAll();
+    }
+
+    private static void searchAll() throws IOException {
+        // 创建客户端对象
+        HttpHost httpHost = new HttpHost("175.24.228.216", 9200, "http");
+        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(httpHost));
+
+        // 创建搜索请求对象
+        SearchRequest request = new SearchRequest();
+        request.indices("student");
+        // 构建查询的请求体
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        // 查询所有数据
+        sourceBuilder.query(QueryBuilders.matchAllQuery());
+        request.source(sourceBuilder);
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 查询匹配
+        SearchHits hits = response.getHits();
+        System.out.println("took:" + response.getTook());
+        System.out.println("timeout:" + response.isTimedOut());
+        System.out.println("total:" + hits.getTotalHits());
+        System.out.println("MaxScore:" + hits.getMaxScore());
+        System.out.println("hits========>>");
+        for (SearchHit hit : hits) {
+        //输出每条查询的结果信息
+            System.out.println(hit.getSourceAsString());
+        }
+        System.out.println("<<========");
+        // 关闭客户端连接
+        client.close();
+    }
+}
